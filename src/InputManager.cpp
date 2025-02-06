7b5390b77667f0841b301f08284dafcf909a7382
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-02-06 22:43:49 +0200

    feat(InputManager): added InputManager class

---
+++ src/InputManager.cpp
@@ -0,0 +1,87 @@
+#include "InputManager.h"
+#include <iostream>
+
+InputManager::InputManager() {
+	initializeKeyStates();
+	initializeMouseButtonStates();
+	initializeMousePositions();
+}
+
+void InputManager::update(const sf::Event& event, const sf::RenderWindow& window) {
+	// update key states
+	if (event.type == sf::Event::KeyPressed) {
+		current_key_states[event.key.code] = true; 
+	} else if (event.type == sf::Event::KeyReleased) {
+		current_key_states[event.key.code] = false;
+	}
+
+	// update mouse states
+	if (event.type == sf::Event::MouseButtonPressed) {
+		current_mouse_states[event.mouseButton.button] = true;
+	} else if (event.type == sf::Event::MouseButtonReleased) {
+		current_mouse_states[event.mouseButton.button] = false;
+	}
+
+	// update mouse position
+	current_mouse_position = sf::Mouse::getPosition(window);
+}
+
+void InputManager::endFrame() {
+	previous_key_states = current_key_states;
+	previous_mouse_states = current_mouse_states;
+	previous_mouse_position = current_mouse_position;
+}
+
+bool InputManager::isKeyJustPressed(const sf::Keyboard::Key& key) const {
+	return current_key_states.at(key) && !previous_key_states.at(key);
+}
+
+bool InputManager::isKeyHeld(const sf::Keyboard::Key& key) const {
+	return current_key_states.at(key) && previous_key_states.at(key);
+}
+
+bool InputManager::wasKeyReleased(const sf::Keyboard::Key& key) const {
+	return !current_key_states.at(key) && previous_key_states.at(key);
+}
+
+bool InputManager::isMouseButtonJustPressed(const sf::Mouse::Button& button) const {
+	return current_mouse_states.at(button) && !previous_mouse_states.at(button);
+}
+
+bool InputManager::isMouseButtonHeld(const sf::Mouse::Button& button) const {
+	return current_mouse_states.at(button) && previous_mouse_states.at(button);
+}
+
+bool InputManager::wasMouseButtonReleased(const sf::Mouse::Button& button) const {
+	return !current_mouse_states.at(button) && previous_mouse_states.at(button);
+}
+
+sf::Vector2i InputManager::getMousePosition() const {
+	return current_mouse_position;
+}
+
+sf::Vector2i InputManager::getMouseDelta() const {
+	return current_mouse_position - previous_mouse_position;
+}
+
+bool InputManager::hasMouseMoved() const {
+	return getMouseDelta() != sf::Vector2i(0, 0);
+}
+
+void InputManager::initializeKeyStates() {
+	for (int i = 0; i < sf::Keyboard::KeyCount; i++) {
+		current_key_states[static_cast<sf::Keyboard::Key>(i)] = false;
+	}
+	previous_key_states = current_key_states;
+}
+
+void InputManager::initializeMouseButtonStates() {
+	for (int i = 0; i < sf::Mouse::ButtonCount; i++) {
+		current_mouse_states[static_cast<sf::Mouse::Button>(i)] = false;
+	}
+	previous_mouse_states = current_mouse_states;
+}
+
+void InputManager::initializeMousePositions() {
+	current_mouse_position = previous_mouse_position = sf::Vector2i(0, 0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include "AssetManager.h"
+#include "InputManager.h"
 
 int main() {
     // Updated RenderWindow constructor
     sf::RenderWindow window(sf::VideoMode(800, 600, 32), "SFML Window");
+    window.setFramerateLimit(5); // set frame rate to 5 fps for debugging purposes
 
+    InputManager input;
     // Create a shape (no change here)
     sf::CircleShape shape(100.0f);
     shape.setFillColor(sf::Color::Green);
 
     // ------- Testing AssetManager
-
     // Load Assets
     AssetManager& assets = AssetManager::getInstance();
     assets.loadAssets("assets/images/");
@@ -24,13 +27,51 @@ int main() {
 
     while (window.isOpen()) {
         sf::Event event;
+        
+
         while (window.pollEvent(event)) {
             // Updated event handling
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+
+            input.update(event, window);
         }
 
-        // Clear, draw, and display the window (no changes)
+        // ------- Testing InputManager
+        if (input.isKeyJustPressed(sf::Keyboard::Key::A)) {
+            std::cout << "A key was just pressed!" << std::endl;
+        }
+
+        if (input.isKeyHeld(sf::Keyboard::Key::A)) {
+            std::cout << "A key is held down!" << std::endl;
+        }
+
+        if (input.wasKeyReleased(sf::Keyboard::Key::A)) {
+            std::cout << "A was just released" << std::endl;
+        }
+
+        if (input.isMouseButtonJustPressed(sf::Mouse::Button::Left)) {
+            std::cout << "Left was just pressed!" << std::endl;
+        }
+
+        if (input.isMouseButtonHeld(sf::Mouse::Button::Left)) {
+            std::cout << "Left is held down!" << std::endl;
+        }
+
+        if (input.wasMouseButtonReleased(sf::Mouse::Button::Left)) {
+            std::cout << "Left was just released" << std::endl;
+        }
+
+        if (input.hasMouseMoved()) {
+            std::cout << "mouse moved" << std::endl;
+        }
+        
+        auto mouse_position = input.getMousePosition();
+        std::cout << "Mouse position: (x) " << mouse_position.x << " (y) " << mouse_position.y << std::endl;
+        // ------- End of Testing InputManager
+
+        input.endFrame();
         window.clear();
         window.draw(test); // Part of testing the AssetManager
         window.display();
