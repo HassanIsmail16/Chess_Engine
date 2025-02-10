3f00054e88166feab00f3453350cca8777291329
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-02-10 03:14:22 +0200

    feat(Board & Movement): added board utilities

---
+++ src/core/game/BoardUtilities.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class Piece;
+
+enum class ChessColor {
+    White,
+    Black
+};
+
+struct Position {
+    Position(const int& row, const int& col): row(row), col(col) {}
+    const int row;
+    const int col;
+};
+
+enum class MoveType {
+    Regular,
+    KingSideCastle,
+    QueenSideCastle,
+    EnPassant
+};
+
+struct Move {
+    Move(const Position& from, const Position& to, Piece* taken_over, const MoveType& type = MoveType::Regular): from(from), to(to), taken_over(taken_over), type(type) {}
+    const Position from;
+    const Position to;
+    Piece* taken_over;
+    const MoveType type;
+};
